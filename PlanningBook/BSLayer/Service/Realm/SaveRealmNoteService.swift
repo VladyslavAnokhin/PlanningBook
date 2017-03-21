6dda9c9f5086633723aaee6c860bc9ce69fa5adf
vladyslav.anokhin <vladislav.anokhin@ddi-dev.com.ua>
2017-03-21 18:52:54 +0200

    added save realm note service

---
+++ PlanningBook/BSLayer/Service/Realm/SaveRealmNoteService.swift
@@ -0,0 +1,44 @@
+//
+//  SaveRealmNoteService.swift
+//  PlanningBook
+//
+//  Created by vladyslav.anokhin on 3/21/17.
+//  Copyright © 2017 DDi Development. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class SaveRealmNoteService: NSObject {
+    
+    var realm: Realm!
+    
+    func save(singleNote note: Note, completion: BoolCompletion){
+        
+        let realmNote = RealmNote()
+        realmNote.title = note.title
+        realmNote.body = note.body
+        
+        // end can't be less than start
+        realmNote.startDate = note.dateRange.start
+        realmNote.endDate = note.dateRange.end
+        
+        if realm == nil {
+            realm = try! Realm()
+        }
+        
+        do {
+            try realm?.write {
+                realm?.add(realmNote)
+                completion(true, nil)
+            }
+        } catch let error {
+            completion(false, error as NSError?)
+        }
+    }
+    
+    func save(arrayOfNotes notes: [Note], completion: BoolCompletion){
+        
+    }
+    
+}
